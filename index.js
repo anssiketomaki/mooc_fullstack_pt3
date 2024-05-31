8ade3de5836098a75edfe269919c1f8924c904de
Anssi Ketomäki <anssi.ketomaki@gmail.com>
2024-05-31 22:55:40 +0300

    troubleshooting 2

--- index.js
+++ index.js
@@ -91,15 +91,11 @@ app.put('/api/persons/:id', (request, response)=>{
   const id = Number(request.params.id)
   const body = request.body
 
-  // if (!body.name || !body.number){
-  //   return response.status(400).json({
-  //     error: 'name or number missing'
-  //   })
-  // } else if (persons.find(p=> p.name !== body.name)){
-  //   return response.status(400).json({
-  //     error: 'name not found to update number'
-  //   })
-  // }
+  if (!body.name || !body.number){
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  } 
  
   const updatedPerson = { ... persons[id], ...body}
   persons[id] = updatedPerson
