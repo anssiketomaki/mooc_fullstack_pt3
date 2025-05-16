e96f5f29166074ab9e2c0f751916bb0555f606c3
Anssi Ketomäki <anssi.ketomaki@tuni.fi>
2025-05-16 17:25:38 +0300

    rm node modules folder from git

--- index.js
+++ index.js
@@ -2,22 +2,127 @@
 const express = require('express')
 const cors = require('cors')
 var morgan = require('morgan')
-
 const mongoose = require('mongoose')
-const Note = require('./models/person')
 
-const url =
-  `mongodb+srv://anssiketomaki:${password}@fspt3.srnnvlt.mongodb.net/?retryWrites=true&w=majority&appName=FSpt3`
+const Persons = require('./models/person')
 
-  mongoose.set('strictQuery', false)
-  mongoose.connect(url)
+const app = express()
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node index.js <password>')
+  process.exit(1)
+  }
+
+const password = process.argv[2]
+const url = `mongodb+srv://anssiketomaki:${password}@fspt3.srnnvlt.mongodb.net/?retryWrites=true&w=majority&appName=FSpt3`
+
+mongoose.set('strictQuery', false)
+console.log('connecting to mongoDB...')
+
+mongoose.connect(url) //add try-catch to catch errors
+  .then(() => {
+    console.log('Connected to MongoDB successfully!')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message)
+    process.exit(1) // Exit if DB connection fails
+  })
+
+// middleware
+app.use(express.json()) //parser for json data
+app.use(express.static('dist')) //serve static files from dist folder
+app.use(cors()) //enable CORS for all origins
+app.use(morgan('tiny')) //log HTTP requests to the console
+
+
+app.get('/', (request, response)=>{
+  response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (request, response)=>{
+  // response.json(persons)
+  Persons.find({}).then(persons =>{
+    response.json(persons)
+  })
+})
+
+app.get('/api/persons/:id', (request, response)=>{
+  const id = Number(request.params.id);
+  const num = Persons.find(person => person.id === id)
+
+  if (num){
+    response.json(num)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) =>{
+  const nums = Persons.length;
+  const datetime = new Date().toString();
+  response.send(`Phonebook has info for ${nums} people<br>${datetime}`) 
+})
+
+app.delete('/api/persons/:id', (request, response)=>{
+  const id = Number(request.params.id)
+  console.log(`poistetaan: ${id}`)
+  Persons = Persons.filter(person => Number(person.id) !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) =>{
+  const body = request.body
+  console.log(body)
+
+  if (!body.name || !body.number){
+    return response.status(400).json({
+      error: 'name or number missing'
     })
+  } else if (Persons.find(p=> p.name === body.name)){
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
 
-    const Person = mongoose.model('Person', personSchema)
+  const newId = Math.floor(Math.random() *32000)
+  body.id = newId.toString()
+
+  Persons = Persons.concat(body)
+
+  response.json(body)
+})
+
+app.put('/api/persons/:id', (request, response)=>{
+  //const id = Number(request.params.id)
+  const body = request.body
+  //console.log(`päivitetään: alkup->${Persons.find(p => p.id === id).number} henkilöön->${body.number}`)
+
+  if (!body.name || !body.number){
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  } 
+
+  const personToUpdate = persons.find(p => p.id === body.id)
+  if (!personToUpdate) {
+    return response.status(404).json({
+      error: 'person not found'
+    });
+  }
+
+  const updatedPerson = { ...personToUpdate, number: body.number}
+  Persons = Persons.filter(person => Number(person.id) !== Number(body.id))
+  Persons = Persons.concat(updatedPerson)
+
+  response.json(updatedPerson)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
+})
+
+  // const Person = mongoose.model('Person', personSchema)
 
 
 // let persons =[
@@ -42,98 +147,3 @@ const url =
 //       "id": "4"
 //     }
 // ]
-
-const app = express()
-app.use(express.json())
-app.use(express.static('dist'))
-app.use(cors())
-app.use(morgan('tiny'))
-
-
-app.get('/', (request, response)=>{
-  response.send('<h1>Hello World</h1>')
-})
-
-app.get('/api/persons', (request, response)=>{
-  // response.json(persons)
-  Person.find({}).then(persons =>{
-    response.json(persons)
-  })
-})
-
-app.get('/api/persons/:id', (request, response)=>{
-  const id = Number(request.params.id);
-  const num = persons.find(person => person.id === id)
-
-  if (num){
-    response.json(num)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.get('/info', (request, response) =>{
-  const nums = persons.length;
-  const datetime = new Date().toString();
-  response.send(`Phonebook has info for ${nums} people<br>${datetime}`) 
-})
-
-app.delete('/api/persons/:id', (request, response)=>{
-  const id = Number(request.params.id)
-  console.log(`poistetaan: ${id}`)
-  persons = persons.filter(person => Number(person.id) !== id)
-  response.status(204).end()
-})
-
-app.post('/api/persons', (request, response) =>{
-  const body = request.body
-  console.log(body)
-
-  if (!body.name || !body.number){
-    return response.status(400).json({
-      error: 'name or number missing'
-    })
-  } else if (persons.find(p=> p.name === body.name)){
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const newId = Math.floor(Math.random() *32000)
-  body.id = newId.toString()
-
-  persons = persons.concat(body)
-
-  response.json(body)
-})
-
-app.put('/api/persons/:id', (request, response)=>{
-  //const id = Number(request.params.id)
-  const body = request.body
-  //console.log(`päivitetään: alkup->${persons.find(p => p.id === id).number} henkilöön->${body.number}`)
-
-  if (!body.name || !body.number){
-    return response.status(400).json({
-      error: 'name or number missing'
-    })
-  } 
-
-  const personToUpdate = persons.find(p => p.id === body.id)
-  if (!personToUpdate) {
-    return response.status(404).json({
-      error: 'person not found'
-    });
-  }
-
-  const updatedPerson = { ...personToUpdate, number: body.number}
-  persons = persons.filter(person => Number(person.id) !== Number(body.id))
-  persons = persons.concat(updatedPerson)
-
-  response.json(updatedPerson)
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=>{
-  console.log(`Server running on port ${PORT}`)
-})
-
